25b53b59f4115817691e158fa50cf1db06cda14f
Leonid Kuzmychenko <lleonidk@ukr.net>
2023-01-13 02:26:32 +0200

    add actuator

--- src/main/java/truthordare/service/QuestionsReaderService.java
+++ src/main/java/truthordare/service/QuestionsReaderService.java
@@ -1,74 +1,52 @@
 package truthordare.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.AllArgsConstructor;
-import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.stereotype.Service;
 import truthordare.model.QuestionEntity;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.Collection;
-import java.util.List;
+import java.io.IOException;
+import java.net.URL;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static ch.qos.logback.core.util.LocationUtil.CLASSPATH_SCHEME;
+
 @Service
 @AllArgsConstructor
 public class QuestionsReaderService {
 
+    private final ResourcePatternResolver resourcePatternResolver;
 
-    private final ResourceLoader resourceLoader;
-
-    public List<QuestionEntity> read() {
-
-//        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("questions");
-//        File file = new File()
-
-        return Stream.of("questions/level_1.json",
-                        "questions/level_2.json",
-                        "questions/level_3.json",
-                        "questions/level_4.json",
-                        "questions/level_5.json")
-                .map(this::read)
+    public List<QuestionEntity> read() throws IOException {
+        Resource[] questionsArray = resourcePatternResolver.getResources(CLASSPATH_SCHEME + "questions/*");
+        return Stream.of(questionsArray).map(this::read)
+                .filter(Objects::nonNull)
+                .map(List::of)
                 .flatMap(Collection::stream)
                 .collect(Collectors.toList());
     }
 
-
-    public String readOne(String path) {
-        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream(path);
-        if (inputStream != null) {
-            String collect = new BufferedReader(new InputStreamReader(inputStream)).lines().collect(Collectors.joining(""));
-            return collect;
-        } else {
-            return null;
-        }
-    }
-
-    public List<QuestionEntity> read(String path) {
+    public QuestionEntity[] read(Resource resource) {
+        Integer level = readNumberFromFileName(resource.getFilename());
         try {
-            String json = readOne(path);
-            QuestionEntity[] questionEntities = new ObjectMapper().readValue(json, QuestionEntity[].class);
-            Integer level = readNumberFromFileName(path);
-            for (QuestionEntity questionEntity : questionEntities) {
-                questionEntity.setLevel(level);
+            QuestionEntity[] questions = new ObjectMapper().readValue(resource.getURL(), QuestionEntity[].class);
+            for (QuestionEntity question : questions) {
+                question.setLevel(level);
             }
-            return List.of(questionEntities);
-        } catch (JsonProcessingException e) {
+            return questions;
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-
-    public Integer readNumberFromFileName(String path) {
-        Pattern pattern = Pattern.compile("(?<=level_)\\d+(?=\\.json)");
-        Matcher matcher = pattern.matcher(path);
+    public Integer readNumberFromFileName(String filename) {
+        Matcher matcher = Pattern.compile("(?<=level_)\\d+(?=\\.json)").matcher(filename);
         String value = matcher.results().findFirst().orElseThrow(() -> new RuntimeException("file name does not contain level")).group();
         try {
             return Integer.parseInt(value);
